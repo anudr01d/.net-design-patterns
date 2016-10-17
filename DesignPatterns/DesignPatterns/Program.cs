907562c2cdc7585cef0bde8d087464a98673ab95
anudeep <anudr01d@users.noreply.github.com>
2016-10-17 17:52:21 +0530

    initial commit

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Beyond veils of darkness.");
+
+            Console.WriteLine("Press a key to exit..");
+            Console.ReadKey();
+        }
+    }
+}

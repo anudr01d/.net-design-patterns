737ff4db510ee328e0e9c26955574e3c6b932ed5
anudeep <anudr01d@users.noreply.github.com>
2016-10-17 20:36:08 +0530

    Added about/further reading file

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -9,6 +9,10 @@ namespace DesignPatterns
     {
         public static void Main(string[] args)
         {
+            /*
+             * Greetings! This is the entry point for executing all the design patterns mentioned within this project.
+             * Simply set the appropriate enum to view the results for the specific design pattern.
+             */ 
             Console.WriteLine("Beyond veils of darkness.");
 
             Console.WriteLine("Press a key to exit..");
